aa7b5ba5af0f0f7004bb008e3df8bcd6f04b1237
vpravd1@gmail.com <Vv77100369>
2021-03-29 22:02:48 +0300

    Task 1

--- victor-pravdivets/src/by/epamtc/task1/ex7/point/Point.java
+++ victor-pravdivets/src/by/epamtc/task1/ex7/point/Point.java
@@ -39,6 +39,40 @@ public class Point {
 		this.name = name;
 	}
 
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return getClass().getSimpleName()+ " " + name + " x=" + x + ", y=" + y;
