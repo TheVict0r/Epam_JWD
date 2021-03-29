2d6cdc2e063b45d09f064091b3b2820476b0c59b
vpravd1@gmail.com <Vv77100369>
2021-03-29 18:59:28 +0300

    Task 1

--- victor-pravdivets/src/by/epamtc/task1/ex6/logic/LogicHoursMinutesSeconds.java
+++ victor-pravdivets/src/by/epamtc/task1/ex6/logic/LogicHoursMinutesSeconds.java
@@ -10,21 +10,18 @@ public static final int MINUTES_IN_HOUR = 60;
 	public static int calculateHours(int secondsBeforeN) {
 		int hours;
 		hours = secondsBeforeN / SECONDS_IN_MINUTE / MINUTES_IN_HOUR;
-		
 		return hours;
 	}
 	
 	public static int calculateMinutes(int secondsBeforeN) {
 		int minutes;
 		minutes = secondsBeforeN / SECONDS_IN_MINUTE % MINUTES_IN_HOUR; 
-		
 		return minutes;
 	}
 	
 	public static int calculateSeconds(int secondsBeforeN) {
 		int seconds;
 		seconds = secondsBeforeN % SECONDS_IN_MINUTE % MINUTES_IN_HOUR; 
-		
 		return seconds;
 	}
 	
