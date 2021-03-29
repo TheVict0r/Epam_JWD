26d6c3ff3c8b6e6faab8ca48dcc4e714ef9b4dcc
vpravd1@gmail.com <Vv77100369>
2021-03-29 18:34:26 +0300

    Task 1

--- victor-pravdivets/src/by/epamtc/task1/ex2/main/Main.java
+++ victor-pravdivets/src/by/epamtc/task1/ex2/main/Main.java
@@ -4,7 +4,8 @@ import by.epamtc.task1.ex2.datarequest.DataRequest;
 import by.epamtc.task1.ex2.logic.YearAndMonthLogic;
 import by.epamtc.task1.ex2.printer.Printer;
 
-/* Составить программу, которая по заданным году и номеру месяца 
+/* 
+ * Составить программу, которая по заданным году и номеру месяца 
  * определяет количество дней в этом месяце и корректно определялись все високосные года.
  */
 
