5b02ac9b823240131bf8c960093c8b3905a49ca8
vpravd1@gmail.com <Vv77100369>
2021-04-15 22:55:49 +0300

    Refactoring

--- victor-pravdivets/src/by/epamtc/task1/ex3/logic/SquareRoundLogic.java
+++ victor-pravdivets/src/by/epamtc/task1/ex3/logic/SquareRoundLogic.java
@@ -2,6 +2,12 @@ package by.epamtc.task1.ex3.logic;
 
 public class SquareRoundLogic {
 	public static double calculateInnerSquareArea(double outerSquareArea) {
+		
+		if (outerSquareArea <= 0) {
+			//throw new IncorrectDataException(outerSquareArea);
+			//пока не реализовано
+		}
+		
 		double innnerSquareDiagonal;
 		innnerSquareDiagonal = calculateSquareSideByArea(outerSquareArea); //innnerSquareDiagonal == outerSquareSide
 
@@ -15,24 +21,46 @@ public class SquareRoundLogic {
 	}
 
 	public static double compareSquaresAreas(double smallerArea, double biggerArea) {
+		if (smallerArea <= 0) {
+			//throw new IncorrectDataException(smallerArea);
+			//пока не реализовано
+		}
+
+		if (biggerArea <= 0) {
+			//throw new IncorrectDataException(biggerArea);
+			//пока не реализовано
+		}
+		
 		double result;
 		result = biggerArea / smallerArea;
 		return result;
 	}
 
 	public static double calculateSquareSideByArea(double area) {
+		if (area <= 0) {
+			//throw new IncorrectDataException(area);
+			//пока не реализовано
+		}
 		double result;
 		result = Math.sqrt(area);
 		return result;
 	}
 
 	public static double calculateSquareAreaByside(double side) {
+		if (side <= 0) {
+			//throw new IncorrectDataException(side);
+			//пока не реализовано
+		}
 		double result;
 		result = side * side;
 		return result;
 	}
 
 	public static double calculateSquareSideByDiagonal(double diagonal) {
+		if (diagonal <= 0) {
+			//throw new IncorrectDataException(diagonal);
+			//пока не реализовано
+		}
 		double result;
 		result = Math.sqrt(diagonal * diagonal / 2);
 		return result;
