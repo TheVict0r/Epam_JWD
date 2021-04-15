235072826b19ef7eb0da8bedb904063e4c418a15
vpravd1@gmail.com <Vv77100369>
2021-04-15 23:27:11 +0300

    Refactoring

--- victor-pravdivets/src/by/epamtc/task1/ex9/logic/CircleLogic.java
+++ victor-pravdivets/src/by/epamtc/task1/ex9/logic/CircleLogic.java
@@ -3,12 +3,24 @@ package by.epamtc.task1.ex9.logic;
 public class CircleLogic {
 
 	public static double calculateLength(int r) {
+		
+		if(r <= 0) {
+			//throw new IncorrectRadiousException(r);
+			//пока не реализовано
+		}
+		
 		double length;
 		length = 2 * Math.PI * r;
 		return length;
 	}
 
 	public static double calculateArea(int r) {
+		
+		if(r <= 0) {
+			//throw new IncorrectRadiousException(r);
+			//пока не реализовано
+		}
+		
 		double area;
 		area = Math.PI * r * r;
 		return area;
